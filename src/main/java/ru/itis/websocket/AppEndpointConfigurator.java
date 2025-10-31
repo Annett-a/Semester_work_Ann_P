e84f828fcfc1286bd2dfb83e983b41b87834f0ea
Annett-a <solo280306@mail.ru>
2025-10-31 20:55:05 +0300

    fix

--- src/main/java/ru/itis/websocket/AppEndpointConfigurator.java
+++ src/main/java/ru/itis/websocket/AppEndpointConfigurator.java
@@ -3,20 +3,14 @@ package ru.itis.websocket;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.http.HttpSession;
 import jakarta.websocket.server.HandshakeRequest;
-import jakarta.websocket.server.HandshakeResponse;
+import jakarta.websocket.HandshakeResponse;
 import jakarta.websocket.server.ServerEndpointConfig;
 
 public class AppEndpointConfigurator extends ServerEndpointConfig.Configurator {
-
     @Override
-    public void modifyHandshake(ServerEndpointConfig config,
-                                HandshakeRequest request,
-                                HandshakeResponse response) {
-
-        Object sessionObj = request.getHttpSession();
-        if (sessionObj instanceof HttpSession) {
-            HttpSession http = (HttpSession) sessionObj;
-
+    public void modifyHandshake(ServerEndpointConfig config, HandshakeRequest request, HandshakeResponse response) {
+        HttpSession http = (HttpSession) request.getHttpSession();
+        if (http != null) {
             config.getUserProperties().put("httpSession", http);
             ServletContext ctx = http.getServletContext();
             config.getUserProperties().put("servletContext", ctx);
